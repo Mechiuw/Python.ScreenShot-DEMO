f8aef3df968476dd1905d166ccb77059b329d292
Matthew Diamonda <144647347+Mechiuw@users.noreply.github.com>
2024-08-05 11:45:10 +0700

    [FEATURE] Added Screenshot features with pillow ui

--- App.py
+++ App.py
@@ -1,15 +1,18 @@
 import pyscreenshot as ImageGrab
 import tkinter as tk
 from tkinter import filedialog, messagebox
+from PIL import Image
 
-root = tk.Tk
+root = tk.Tk()
 root.title("SSAP")
 root.geometry("300x150")
 
 # take ss function
 def takeScreenShot():
     root.withdraw
-    screenshot = ImageGrab.grab()
+    
+    bbox = None
+    screenshot = ImageGrab.grab(bbox=bbox)
     
     root.deiconify()
     
@@ -18,11 +21,11 @@ def takeScreenShot():
                                          filetypes=[("PNG files","*.png"),("All files","*.*")])
     
     if file_path:
-        screenshot.save(file_path)
+        screenshot.save(file_path, format="JPEG")
         messagebox.showinfo("Screenshot saved", f"Screenshot saved to {file_path}")
         
 
-screenshot_button = tk.Button(root,text="take ss", command=takeScreenShot)
+screenshot_button = tk.Button(root,text="screenshot", command=takeScreenShot)
 screenshot_button.pack(expand=True)
 
 root.mainloop()
