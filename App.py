828d7c61b21df18e40bd075a408b96c8f6c0275e
Matthew Diamonda <144647347+Mechiuw@users.noreply.github.com>
2024-08-05 11:59:40 +0700

    [FEATURE] Added Exceptions to takeScreenShot functions

--- App.py
+++ App.py
@@ -9,24 +9,32 @@ root.geometry("300x150")
 
 # take ss function
 def takeScreenShot():
-    root.withdraw
-    
-    # ss field dimesnsions range  
-    bbox = None
-    screenshot = ImageGrab.grab(bbox=bbox)
-    
-    root.deiconify()
-    
-    # configure file path and format
-    file_path = filedialog.asksaveasfile(defaultextension=".png",
-                                         filetypes=[("PNG files","*.png"),
-                                                    ("JPEG files","*.jpeg"),
-                                                    ("All files","*.*")])
-    
-    if file_path:
-        file_format = file_path.split('.')[-1].upper()
-        screenshot.save(file_path, format=file_format)
-        messagebox.showinfo("Screenshot saved", f"Screenshot saved to {file_path}")
+    try:
+        root.withdraw
+        
+        # ss field dimesnsions range  
+        bbox = None
+        screenshot = ImageGrab.grab(bbox=bbox)
+        
+        root.deiconify()
+        
+        # configure file path and format
+        file_path = filedialog.asksaveasfile(defaultextension=".png",
+                                            filetypes=[("PNG files","*.png"),
+                                                        ("JPEG files","*.jpeg"),
+                                                        ("All files","*.*")])
+        
+        if file_path:
+            file_format = file_path.split('.')[-1].upper()
+            screenshot.save(file_path, format=file_format)
+            messagebox.showinfo("Screenshot saved", f"Screenshot saved to {file_path}")
+        else :
+            retry = messagebox.askyesnocancel("Retry", "take another screenshot?")
+            if retry :
+                takeScreenShot()
+    except Exception as e:
+        if messagebox.askretrycancel("Error",f"An error occurred: {e}\nWould you like to try again?"):
+            takeScreenShot()
         
 
 screenshot_button = tk.Button(root,text="screenshot", command=takeScreenShot)
